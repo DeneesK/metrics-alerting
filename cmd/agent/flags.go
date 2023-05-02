fb2c86b6e46ecdd34fa5fcea5115de138703709a
D Kalb <kds-ru@list.ru>
2023-05-02 11:05:19 +0200

    env

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strconv"
 )
 
 var flagRunAddr string
@@ -13,4 +15,15 @@ func parseFlags() {
 	flag.IntVar(&flagreportInterval, "r", 10, "override reportInterval - the frequency of sending metrics to the server")
 	flag.IntVar(&flagpolltInterval, "p", 2, "override pollInterval - the frequency of polling metrics from the runtime package")
 	flag.Parse()
+	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
+	if envreportInterval := os.Getenv("REPORT_INTERVAL"); envreportInterval != "" {
+		fri, _ := strconv.Atoi(envreportInterval)
+		flagreportInterval = fri
+	}
+	if envpolltInterval := os.Getenv("POLL_INTERVAL "); envpolltInterval != "" {
+		fpi, _ := strconv.Atoi(envpolltInterval)
+		flagpolltInterval = fpi
+	}
 }
