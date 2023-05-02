b6b0adb773bc4f702f941a2dfe93ee588456691c
D Kalb <kds-ru@list.ru>
2023-05-02 10:37:14 +0200

    agent urlpreparer

--- cmd/agent/urlpreparer/preparer.go
+++ cmd/agent/urlpreparer/preparer.go
@@ -8,7 +8,7 @@ import (
 
 func PrepareURL(basePath string, metricType string, metricName string, value float64) string {
 	v := fmt.Sprintf("%f", value)
-	u, err := url.JoinPath(basePath, "update", metricType, metricName, v)
+	u, err := url.JoinPath("http://", basePath, "update", metricType, metricName, v)
 	if err != nil {
 		log.Println(err)
 		return ""
