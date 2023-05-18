176260815a2dea58ce9c1c80452a327b923af7ac
D Kalb <kds-ru@list.ru>
2023-05-18 10:45:27 +0200

    logger added

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -11,10 +11,12 @@ func main() {
 	if err := parseFlags(); err != nil {
 		log.Fatal(err)
 	}
+
 	reportInterval := time.Duration(reportingInterval) * time.Second
 	ms := metriccollector.NewCollector(pollingInterval)
 	go ms.StartCollect()
 	log.Printf("client started sending data on %s", runAddr)
+
 	for {
 		if err := sendMetrics(&ms); err != nil {
 			log.Println(err)
